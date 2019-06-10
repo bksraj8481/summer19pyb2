bec9dca00d039f57a69a3edcd1bbbd242e6f64e8
bksraj8481 <bksraj8481@gmail.com>
2019-06-11 00:45:17 +0530

    solution

---
+++ first.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import datetime
+
+a, b = input("Enter your name and age: ").split()
+print("Hey {}, You'll turn 95 in ".format(a) +str(datetime.date.today().year + 95 - int(b)) + '.')
+
+
